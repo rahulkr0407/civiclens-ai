306a8d1e8e1913b097ffaacefd1775ae47011593
Rahul <mrahul2572002@gmail.com>
2026-07-29 19:35:18 +0530

    feat: configure Angular 20 with Tailwind CSS

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './features/home/pages/home/home';
 
-export const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-  },
-];
+export const routes: Routes = [];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
+  styleUrl: './app.scss'
 })
-export class App {}
+export class App {
+  protected readonly title = signal('frontend');
+}
