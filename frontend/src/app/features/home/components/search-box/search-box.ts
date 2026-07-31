ea4e14d7b9663871c61d4cb36b019a1d60b44624
Rahul <mrahul2572002@gmail.com>
2026-07-31 08:28:18 +0530

    feat: implement search functionality with query parameters

--- frontend/src/app/features/home/components/search-box/search-box.ts
+++ frontend/src/app/features/home/components/search-box/search-box.ts
@@ -1,19 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-box',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search-box.html',
   styleUrl: './search-box.scss',
 })
 export class SearchBoxComponent {
 
+  searchText = '';
+
   constructor(private router: Router) {}
 
   search() {
-    this.router.navigate(['/search']);
+    this.router.navigate(['/search'], {
+      queryParams: {
+        q: this.searchText,
+      },
+    });
   }
 
 }
--- frontend/src/app/features/search/pages/search-results/search-results.ts
+++ frontend/src/app/features/search/pages/search-results/search-results.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { SearchService } from '../../../../core/services/search';
 
 @Component({
@@ -9,11 +10,34 @@ import { SearchService } from '../../../../core/services/search';
   styleUrl: './search-results.scss',
 })
 export class SearchResults {
-
   topics: any[] = [];
+  allTopics: any[] = [];
 
-  constructor(private searchService: SearchService) {
-    this.topics = this.searchService.getTopics();
+  constructor(
+    private searchService: SearchService,
+    private route: ActivatedRoute,
+  ) {
+    this.allTopics = this.searchService.getTopics();
+
+    this.route.queryParams.subscribe((params) => {
+      console.log('Params:', params);
+
+      const searchText = (params['q'] || '').toLowerCase();
+
+      console.log('Search Text:', searchText);
+
+      if (!searchText) {
+        this.topics = this.allTopics;
+        return;
+      }
+
+      console.log('All Topics:', this.allTopics);
+
+      this.topics = this.allTopics.filter((topic) =>
+        topic.title.toLowerCase().includes(searchText),
+      );
+
+      console.log('Filtered Topics:', this.topics);
+    });
   }
-
 }
