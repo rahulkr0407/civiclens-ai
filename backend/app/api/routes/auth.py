f8027717834b317c8a2c6b4bea6c2406f1089f23
Rahul <mrahul2572002@gmail.com>
2026-08-08 02:16:11 +0530

    changes in auth-py

--- backend/app/api/routes/auth.py
+++ backend/app/api/routes/auth.py
@@ -23,35 +23,8 @@ class LoginRequest(BaseModel):
     password: str
 
 
-# @router.post("/signup")
-# def signup(user: SignupRequest):
-
-#     existing_user = users_collection.find_one({
-#         "email": user.email
-#     })
-
-#     if existing_user:
-#         raise HTTPException(
-#             status_code=400,
-#             detail="An account with this email already exists.",
-#         )
-
-#     hashed_password = password_hash.hash(user.password)
-
-#     users_collection.insert_one({
-#         "fullName": user.fullName,
-#         "email": user.email,
-#         "password": hashed_password,
-#     })
-
-#     return {
-#         "message": "Account created successfully."
-#     }
-
 @router.post("/signup")
 def signup(user: SignupRequest):
-
-    print("SIGNUP DATA:", user)
 
     existing_user = users_collection.find_one({
         "email": user.email
@@ -65,23 +38,14 @@ def signup(user: SignupRequest):
 
     hashed_password = password_hash.hash(user.password)
 
-    result = users_collection.insert_one({
+    users_collection.insert_one({
         "fullName": user.fullName,
         "email": user.email,
         "password": hashed_password,
     })
 
-    print("INSERTED ID:", result.inserted_id)
-
-    saved_user = users_collection.find_one({
-        "_id": result.inserted_id
-    })
-
-    print("SAVED USER:", saved_user)
-
     return {
-        "message": "Signup successful",
-        "user_id": str(result.inserted_id)
+        "message": "Account created successfully."
     }
 
 
